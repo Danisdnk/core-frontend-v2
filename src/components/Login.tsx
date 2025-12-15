fd4b9381881fe7a0f7daf59a9d31614d4db03161
Gregorio Carranza <gregoriocarranza@hotmail.com>
2025-12-14 23:34:11 -0300

    fix

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -29,7 +29,7 @@ export default function Login() {
     }
 
     // if (dest) window.location.href = dest; //aca deberia estar el repreguntar si continuar con la sesion antes de mandar a este dest
-    if (!dest) navigate("/home", { replace: true });
+    navigate("/home", { replace: true });
   }, [capturedRedirect, navigate]);
 
   const handleSubmit = async (e: React.FormEvent) => {
